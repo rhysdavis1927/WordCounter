79f3da683937469c891b5f4932a66459bdd79c55
Rhys Davis <rhys.davis@blackpepper.co.uk>
2021-06-06 21:32:12 +0100

    Take in file path and output results

--- src/main/java/WordCounterApp.java
+++ src/main/java/WordCounterApp.java
@@ -1,14 +1,38 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordCounterApp {
 
     public static void main(String[] args) {
-        File file = new File("filename.txt");
+        File file = new File(args[0]);
         try {
             WordCountResult result = new WordCounter().CountWords(file);
+            System.out.println("Word count = " + result.getWordCount());
+            System.out.println("Average word length = " + result.getAverageWordLength());
+            for (Map.Entry<Integer, Integer> countsByWordLength : result.getCountOfEachWordLength().entrySet()) {
+                System.out.println("Number of words of length " + countsByWordLength.getKey() + " is " + countsByWordLength.getValue());
+            }
+            CountWithWordLengths mostFrequentWordLengthsAndCount = result.getMostFrequentWordLengthsAndCount();
+            System.out.println("The most frequently occurring word length is " + mostFrequentWordLengthsAndCount.getCount()
+                    + ", for word lengths of " + getListOfWordLengthsString(mostFrequentWordLengthsAndCount));
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         }
+    }
+
+    private static String getListOfWordLengthsString(CountWithWordLengths countWithWordLengths) {
+        List<String> wordCounts = countWithWordLengths.getWordLengths().stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
+        if (wordCounts.size() == 1) {
+            return wordCounts.get(0);
+        }
+
+        return wordCounts.stream()
+                .limit(wordCounts.size() - 1)
+                .collect(Collectors.joining(", ", "", " & "))
+                .concat(wordCounts.get(wordCounts.size() - 1));
     }
 }
